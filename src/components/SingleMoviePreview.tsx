a6236b70abf13065646541e965a63153cd5e868f
salman1204 <salmanvai38@gmail.com>
2022-11-13 23:57:14 +0600

    add zoom effect on hove in poster

--- src/components/SingleMoviePreview.tsx
+++ src/components/SingleMoviePreview.tsx
@@ -1,6 +1,7 @@
 import { Col } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import { movie } from '../utils/types/movie'
+import styles from './singleMoviePreview.module.css'
 
 interface Props {
   movie: movie
@@ -8,13 +9,13 @@ interface Props {
 
 const SingleMoviePreview: React.FC<Props> = ({ movie }) => {
   return (
-    <Col className="text-center mt-5">
+    <Col className={`text-center mt-5 ${styles.zoom}`}>
       <Link to={`/movies/${movie.id}`} className="text-decoration-none">
         <img
           src={`https://image.tmdb.org/t/p/w154/${movie.poster_path}`}
           alt="BigCo Inc. logo"
         />
-        <h6 className="text-light my-3 mx-2">
+        <h6 className="text-light my-3 mx-3 " >
           {movie.title || movie.original_title}
         </h6>
       </Link>
